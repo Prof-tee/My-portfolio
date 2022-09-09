8cf7f525686818dec1356bdf5233bd84b5f69830
Prof-tee <tonioizunobi@gmail.com>
2022-09-09 13:45:03 +0100

    first commit

---
+++ js/port.js
@@ -0,0 +1,47 @@
+let yr = new Date().getFullYear()
+
+const date = document.getElementById('date');
+
+date.innerHTML = yr;
+// function myClass() {
+//     let name = window.prompt( "what is your favourite  soft drink? ")
+//     if (name == "Fanta" && true) {
+//       window.alert("That's Fantastic")
+//     }
+//     else if (name == "Sprite" && true) {
+//       window.alert("It's also my best drink")
+//     }
+//     else if (name == "Coke" && true) {
+//       window.alert("That's Wonderful")
+//     }
+//     else if (name == "Bigi Bitter Lemon" && true) {
+//       window.alert("That Tastes Terrible")
+//     }
+//     else if (name == "Bigi Tropical" && true) {
+//       window.alert("That's Healthy")
+//     }
+//     else if (name == "Bigi Soda" && true) {
+//       window.alert("That is Tasteless")
+//     }
+//     else if (name == "Bigi Apple" && true) {
+//       window.alert("I Like it too")
+//     }
+//     else if (name == "Fearless" && true) {
+//       window.alert("I Hate It")
+//     }
+//     else if (name == "Bigi Ginger Ale" && true) {
+//       window.alert("That's Nice")
+//     }
+//     else if (name == "Bigi Ginger Lemon" && true) {
+//       window.alert("That's Horrible")
+//     }
+//     else if(name =="PineApple Mirinda" && true){
+//       window.alert("That's Great")
+//     }
+//     else return window.alert("I Have Not Heard Of That Drink Before")
+//   }
+
+
+
+
+
